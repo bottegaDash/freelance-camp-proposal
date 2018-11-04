c1365b1ad75ed01c5f50804a5ef2402a60d4e5fd
David Ash <bottegadash@gmail.com>
2018-11-04 07:42:21 -0500

    Populated proposal with data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |proposal|
+  Proposal.create!(
+    customer: "Customer #{proposal}",
+    portfolio_url: "http://msn.com",
+    tools: "Ruby on Rails, Angular 6, and Prostgress",
+    estimated_hours: (80 + proposal),
+    hourly_rate: 120,
+    weeks_to_complete: 12,
+    client_email: 'davidjash2@gmail.com'
+  )
+end
